fef348ff076abc59ee167801c9182d414fe700cf
Kent Ntwari <designerjehovah@gmail.com>
2024-03-12 05:31:12 -0400

    feat: add new modules

--- code/nuxt.config.ts
+++ code/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  build: {
+    transpile: ['trpc-nuxt']
+  },
+  typescript: {
+    typeCheck: true,
+    strict: true
+  },
+  modules: ["@nuxtjs/tailwindcss", "@vee-validate/nuxt"],
+  veeValidate: {
+    // disable or enable auto imports
+    autoImports: true,
+    // Use different names for components
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray',
+      ErrorMessage: 'VeeErrorMessage',
+    }
+  }
 })
