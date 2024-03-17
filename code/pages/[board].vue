c7bdde811ab0c7ea9b1f00b9acafb953ecf4d318
Kent Ntwari <designerjehovah@gmail.com>
2024-03-17 12:46:07 -0400

    feat: instantiate pages
    create default home route and per board routes

---
+++ code/pages/[board].vue
@@ -0,0 +1,18 @@
+<script
+    setup
+    lang="ts"
+>
+import type { Board } from '~/types';
+
+const route = useRoute()
+
+const { data } = useNuxtData<Board>('boards')
+</script>
+
+
+<template>
+    <BoardTasks
+        v-if="data"
+        :boardId="data.filter(({name})  => name === route.params.board)[0].id"
+    />
+</template>
---
+++ code/pages/index.vue
@@ -0,0 +1,12 @@
+<script
+    setup
+    lang="ts"
+>
+import type { Board } from '~/types';
+
+const { data } = useNuxtData<Board>('boards')
+</script>
+
+<template>
+    <BoardTasks :boardId="!data ? null : data[0].id" />
+</template>
