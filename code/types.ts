7056288b7c466a272f704d6f2c0b1f24c8672462
Kent Ntwari <designerjehovah@gmail.com>
2024-03-25 03:49:58 -0400

    feat: new types added

--- code/types.ts
+++ code/types.ts
@@ -1,5 +1,6 @@
-import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from '@/server/trpc/routers';
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@/server/trpc/routers";
 
-export type Board=inferRouterOutputs<AppRouter>['getBoardNames']
-export type Task = inferRouterOutputs<AppRouter>['getTask']
+export type BoardTasks = inferRouterOutputs<AppRouter>["getBoardTasks"];
+export type Board = inferRouterOutputs<AppRouter>["getBoardNames"];
+export type Task = inferRouterOutputs<AppRouter>["getTask"];
