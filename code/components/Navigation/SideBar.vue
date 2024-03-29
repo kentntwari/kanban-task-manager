d5e53e3fbc6a49b2261db896da9577f9da11cffd
Kent Ntwari <designerjehovah@gmail.com>
2024-03-29 18:47:36 -0400

    feat:init global navigation

---
+++ code/components/Navigation/SideBar.vue
@@ -0,0 +1,29 @@
+<script
+  lang="ts"
+  setup
+>
+const isMobileAsideOpen = useState('is-mobile-aside-open', ()=> ref(false))
+
+const {data:boards}=useBoards()
+
+const boardMenu = ref(null)
+
+onClickOutside(boardMenu, () =>{
+  if(isMobileAsideOpen.value === true){
+    isMobileAsideOpen.value = false
+  }
+})
+
+</script>
+
+<template>
+  <aside
+    v-show="isMobileAsideOpen"
+    class="[grid-area:1/1] fixed left-1/2 -translate-x-1/2 z-30 mt-20 bg-white w-3/4 max-w-[264px] h-fit py-4 space-y-5 rounded-lg"
+  >
+    <span class="block uppercase text-sm tracking-[2.4px] px-6">
+      All Boards ({{boards?.length}})
+    </span>
+    <BoardMenu ref="boardMenu" />
+  </aside>
+</template>
---
+++ code/components/Navigation/TopBar.vue
@@ -0,0 +1,77 @@
+<script
+  lang="ts"
+  setup
+>
+const {data:boards} = useBoards()
+
+const {isCanAddTask} = useAddTask()
+
+const isMobileAsideOpen = useState('is-mobile-aside-open')
+
+const route = useRoute()
+
+const {shouldRefetchBoardData, isAddNewTask, addNewTaskFn} = useFormUtils()
+
+const { isRevealed:isModalOpen, reveal:openModalFn, cancel:closeModalFn } = useConfirmDialog()
+
+</script>
+
+<template>
+  <header
+    class="fixed top-0 z-30 bg-white w-full h-16 px-4 flex items-center justify-between"
+  >
+    <Modal
+      v-model:open="isModalOpen"
+      @interact-outside="closeModalFn()"
+    >
+      <FormTask @create="()=>{
+          shouldRefetchBoardData = true
+          closeModalFn()
+    }">
+        <template #title>
+          <h2
+            v-show="isAddNewTask"
+            class="form-title"
+          >
+            Add New Task
+          </h2>
+        </template>
+      </FormTask>
+    </Modal>
+
+    <section class="flex items-center gap-4">
+      <NuxtLink to="/">
+        <SvgLogo size="sm" />
+      </NuxtLink>
+      <div class="relative flex items-center gap-2">
+        <h3 class="text-xl text-black">
+          {{ route.path === '/' ? boards?.at(0)?.name : route.params.board}}
+        </h3>
+        <button
+          type="button"
+          class="before:absolute before:top-0 before:left-0 before:w-full before:h-full"
+          @click="isMobileAsideOpen =!isMobileAsideOpen"
+        >
+          <SvgIcons icon="chevron-down" />
+        </button>
+      </div>
+    </section>
+
+    <div class="flex items-center gap-4">
+      <button
+        type="button"
+        class="btn-small-primary min-w-12 disabled:bg-main-purple/25"
+        :disabled="isCanAddTask"
+        @click="event =>{
+          openModalFn()
+          addNewTaskFn()
+          }"
+      >
+        <SvgIcons icon="plus" />
+      </button>
+      <BoardPrompt>
+        <SvgIcons icon="dots" />
+      </BoardPrompt>
+    </div>
+  </header>
+</template>
