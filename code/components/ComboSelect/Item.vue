c51bbc043dc3d7d5bc7d6fa5780c5dd8db5bb0ac
Kent Ntwari <designerjehovah@gmail.com>
2024-03-29 18:43:45 -0400

    init comboSelect for dropdown purposes

---
+++ code/components/ComboSelect/Item.vue
@@ -0,0 +1,17 @@
+<script
+  setup
+  lang="ts"
+>
+import type { ComboboxItemProps } from 'radix-vue'
+
+const props = defineProps<ComboboxItemProps>()
+</script>
+
+<template>
+  <ComboboxItem
+    v-bind="props"
+    class="relative py-2 text-md leading-none cursor-pointer"
+  >
+    <slot />
+  </ComboboxItem>
+</template>
---
+++ code/components/ComboSelect/index.vue
@@ -0,0 +1,49 @@
+<script
+  setup
+  lang="ts"
+>
+import {  useForwardPropsEmits } from 'radix-vue'
+import type { ComboboxRootEmits, ComboboxRootProps } from 'radix-vue'
+
+interface ExtendedComboboxRootProps extends ComboboxRootProps {
+  placeholder?: string;
+}
+
+const props = defineProps<ExtendedComboboxRootProps>()
+const emits = defineEmits<ComboboxRootEmits>()
+
+const forward = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <ComboboxRoot
+    v-bind="forward"
+    class="relative cursor-pointer"
+  >
+    <ComboboxAnchor
+      class="w-full inline-flex items-center justify-between rounded px-4 text-md text-black leading-none h-10 border border-medium-grey/25"
+    >
+      <ComboboxInput
+        class="!bg-transparent outline-none text-grass11 h-full selection:bg-grass5 placeholder-mauve8"
+        :placeholder="props.placeholder ?? ''"
+      />
+      <ComboboxTrigger>
+        <SvgIcons icon="chevron-down" />
+      </ComboboxTrigger>
+    </ComboboxAnchor>
+
+    <ComboboxContent
+      class="absolute z-10 w-full mt-2 min-w-[160px] bg-white overflow-hidden rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+    >
+      <ComboboxViewport class="p-[5px]">
+        <ComboboxEmpty class="text-xs font-medium text-center py-2">
+          <slot name="empty">No options</slot>
+        </ComboboxEmpty>
+
+        <ComboboxGroup class="p-4">
+          <slot name="content"></slot>
+        </ComboboxGroup>
+      </ComboboxViewport>
+    </ComboboxContent>
+  </ComboboxRoot>
+</template>
