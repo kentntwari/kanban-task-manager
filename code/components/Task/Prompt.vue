f7bdfd8bea1f61a8a9265ac021481f6d3f7c7716
Kent Ntwari <designerjehovah@gmail.com>
2024-03-31 12:51:02 -0400

    refactor: add PromptColapsible wrapper

---
+++ code/components/Task/Prompt.vue
@@ -0,0 +1,41 @@
+<script
+  setup
+  lang="ts"
+>
+const emit = defineEmits<{
+  promptValidate: [void]
+}>()
+
+const {deleteTaskFn, editTaskFn } = useFormUtils()
+
+const isCollapsed = ref(false)
+</script>
+
+<template>
+  <PromptCollapsible v-model:collapsed="isCollapsed">
+    <template #trigger>
+      <SvgIcons icon="dots" />
+    </template>
+
+    <template #prompts>
+      <div class="flex flex-col gap-4">
+        <button
+          type="button"
+          @click="event => {
+            emit('promptValidate')
+            editTaskFn()
+          }"
+        >Edit Task</button>
+
+        <button
+          type="button"
+          class="text-red"
+          @click="event => {
+            emit('promptValidate')
+            deleteTaskFn()
+          }"
+        >Delete Task</button>
+      </div>
+    </template>
+  </PromptCollapsible>
+</template>
