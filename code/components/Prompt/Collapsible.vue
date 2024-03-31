18e4b6e5936b0461ea449ad5a14b489b209082b2
Kent Ntwari <designerjehovah@gmail.com>
2024-03-31 12:25:57 -0400

    feat: add collapsible component wrapper

---
+++ code/components/Prompt/Collapsible.vue
@@ -0,0 +1,42 @@
+<script
+  setup
+  lang="ts"
+>
+const props = defineProps<{
+  prompts?: string[]
+}>()
+
+const isCollapsed = defineModel<boolean>('collapsed')
+
+const prompt = ref(null)
+
+onClickOutside(prompt, ()=>{
+  if(isCollapsed.value === true){
+    isCollapsed.value = false
+  }
+})
+</script>
+
+<template>
+  <CollapsibleRoot
+    ref="prompt"
+    class="relative"
+    v-model:open="isCollapsed"
+  >
+    <CollapsibleTrigger>
+      <slot name="trigger"></slot>
+    </CollapsibleTrigger>
+
+    <ClientOnly>
+      <CollapsibleContent
+        class="absolute top-10 right-0 p-4 min-w-48 bg-white group:text-md rounded-lg shadow-lg"
+      >
+        <slot
+          name="prompts"
+          :data="props.prompts"
+        ></slot>
+      </CollapsibleContent>
+    </ClientOnly>
+    <slot></slot>
+  </CollapsibleRoot>
+</template>
