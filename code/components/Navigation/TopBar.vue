83da8c73e39a01aa78fd071f3e12475a0ffb8630
Kent Ntwari <designerjehovah@gmail.com>
2024-04-18 22:04:38 -0400

    refactor:

--- code/components/Navigation/TopBar.vue
+++ code/components/Navigation/TopBar.vue
@@ -1,51 +1,31 @@
-<script
-  lang="ts"
-  setup
->
-const {data:boards} = useBoards()
+<script lang="ts" setup>
+  const { data: boards } = useBoards();
 
-const {isCanAddTask} = useAddTask()
+  const { isCanAddTask } = useAddTask();
 
-const isMobileAsideOpen = useState('is-mobile-aside-open')
+  const isMobileAsideOpen = useState("is-mobile-aside-open");
 
-const route = useRoute()
+  const route = useRoute();
 
-const {shouldRefetchBoardData, isAddNewTask, addNewTaskFn} = useFormUtils()
-
+  const { isAddNewTask, addNewTaskFn } = useFormUtils();
 </script>
 
 <template>
   <header
-    class="fixed top-0 z-30 bg-white w-full h-16 px-4 flex items-center justify-between"
+    class="fixed md:static top-0 z-30 bg-white dark:bg-dark-grey w-full h-16 px-4 flex items-center justify-between"
   >
-    <Modal
-      v-model:open="isAddNewTask"
-      @interact-outside="isAddNewTask = false"
-    >
-      <FormTask @create="()=>{
-          shouldRefetchBoardData = true
-          isAddNewTask = false
-    }">
-        <template #title>
-          <h2 class="form-title">
-            Add New Task
-          </h2>
-        </template>
-      </FormTask>
-    </Modal>
-
     <section class="flex items-center gap-4">
-      <NuxtLink to="/">
+      <NuxtLink to="/" class="md:hidden">
         <SvgLogo size="sm" />
       </NuxtLink>
       <div class="relative flex items-center gap-2">
-        <h3 class="text-xl text-black">
-          {{ route.path === '/' ? boards?.at(0)?.name : route.params.board}}
+        <h3 class="text-xl text-black dark:text-white">
+          {{ route.path === "/" ? boards?.at(0)?.name : route.params.board }}
         </h3>
         <button
           type="button"
-          class="before:absolute before:top-0 before:left-0 before:w-full before:h-full"
-          @click="isMobileAsideOpen =!isMobileAsideOpen"
+          class="md:hidden before:absolute before:top-0 before:left-0 before:w-full before:h-full"
+          @click="isMobileAsideOpen = !isMobileAsideOpen"
         >
           <SvgIcons icon="chevron-down" />
         </button>
@@ -55,12 +35,15 @@ const {shouldRefetchBoardData, isAddNewTask, addNewTaskFn} = useFormUtils()
     <div class="flex items-center gap-4">
       <button
         type="button"
+        aria-label="add new task"
         class="btn-small-primary min-w-12 disabled:bg-main-purple/25"
         :disabled="isCanAddTask"
-        @click="event =>{
-          isAddNewTask = true
-          addNewTaskFn()
-          }"
+        @click="
+          (event) => {
+            isAddNewTask = true;
+            addNewTaskFn();
+          }
+        "
       >
         <SvgIcons icon="plus" />
       </button>
