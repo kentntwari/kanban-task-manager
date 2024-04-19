8fa2cc1a6a42e69d40b011c06e2d5bb3620be3de
Kent Ntwari <designerjehovah@gmail.com>
2024-04-18 22:01:16 -0400

    refactor:remove unnecessary cahce

--- code/composables/useBoards.ts
+++ code/composables/useBoards.ts
@@ -1,14 +1,7 @@
 import type { Board } from "~/types";
 
 export default function useBoards() {
-	const { $client, payload, static: nuxtStatic } = useNuxtApp();
+  const { $client, payload, static: nuxtStatic } = useNuxtApp();
 
-	return useAsyncData<Board>("boards", () => $client.getBoardNames.query(), {
-		getCachedData: (key) => {
-			const data = payload.data[key] || nuxtStatic.data[key];
-
-			if (!data) return;
-			return data;
-		},
-	});
+  return useAsyncData<Board>("boards", () => $client.getBoardNames.query());
 }
